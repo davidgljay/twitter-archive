1bbeaa879537bba69c6972dd36da3c0f1813714a
davidgljay <davidgljay@gmail.com>
2011-12-02 19:07:20 -0500

    Upgraded to ver 2

--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -1,6 +1,16 @@
 desc "This task is called by the Heroku cron add-on"
 task :cron => :environment do
   if Time.now.hour == 0 # run at midnight
-    app.get '/sync'
+        @synclist = Hashtag.all.delete_if{|tag| tag.archive == false}
+    secondaries = Array.new
+    @synclist.each do |tag|
+       tag.tweets.each do |tweet|
+         secondaries << tweet.hashtags
+       end
+    end
+    @synclist << secondaries
+    @synclist.flatten!.uniq!.each do |tag|
+       tag.delay.get_tweets
+    end
   end
 end
---
+++ vendor/plugins/delayed_job/lib/delayed/psych_ext.rb
@@ -0,0 +1,75 @@
+if defined?(ActiveRecord)
+  class ActiveRecord::Base
+    # serialize to YAML
+    def encode_with(coder)
+      coder["attributes"] = @attributes
+      coder.tag = ['!ruby/ActiveRecord', self.class.name].join(':')
+    end
+  end
+end
+
+class Delayed::PerformableMethod
+  # serialize to YAML
+  def encode_with(coder)
+    coder.map = {
+      "object" => object,
+      "method_name" => method_name,
+      "args" => args
+    }
+  end
+end
+
+module Psych
+  module Visitors
+    class YAMLTree
+      def visit_Class(klass)
+        tag = ['!ruby/class', klass.name].join(':')
+        register(klass, @emitter.start_mapping(nil, tag, false, Nodes::Mapping::BLOCK))
+        @emitter.end_mapping
+      end
+    end
+
+    class ToRuby
+      def visit_Psych_Nodes_Mapping_with_class(object)
+        return revive(Psych.load_tags[object.tag], object) if Psych.load_tags[object.tag]
+
+        case object.tag
+        when /^!ruby\/class:?(.*)?$/
+          resolve_class $1
+        when /^!ruby\/ActiveRecord:(.+)$/
+          klass = resolve_class($1)
+          payload = Hash[*object.children.map { |c| accept c }]
+          id = payload["attributes"][klass.primary_key]
+          begin
+            if ActiveRecord::VERSION::MAJOR == 3
+              klass.unscoped.find(id)
+            else # Rails 2
+              klass.with_exclusive_scope { klass.find(id) }
+            end
+          rescue ActiveRecord::RecordNotFound
+            raise Delayed::DeserializationError
+          end
+        when /^!ruby\/Mongoid:(.+)$/
+          klass = resolve_class($1)
+          payload = Hash[*object.children.map { |c| accept c }]
+          begin
+            klass.find(payload["attributes"]["_id"])
+          rescue Mongoid::Errors::DocumentNotFound
+            raise Delayed::DeserializationError
+          end
+        else
+          visit_Psych_Nodes_Mapping_without_class(object)
+        end
+      end
+      alias_method_chain :visit_Psych_Nodes_Mapping, :class
+
+      def resolve_class_with_constantize(klass_name)
+        klass_name.constantize
+      rescue
+        resolve_class_without_constantize(klass_name)
+      end
+      alias_method_chain :resolve_class, :constantize
+    end
+  end
+end
+
